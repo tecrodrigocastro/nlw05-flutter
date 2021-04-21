8c699f3c91daf9fdb48b115ac1ae5fd512e57543
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-04-21 13:46:16 -0300

    applying VauleNotifier

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -4,21 +4,28 @@ import 'package:DevQuiz/shared/models/awnser_model.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
+import 'package:flutter/foundation.dart';
 
 class HomeController {
-  HomeState state = HomeState.empty;
-
+  final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
   UserModel? user;
   List<QuizModel>? quizzes;
 
-  void getUser() {
+  void getUser() async {
+    state = HomeState.loading;
+    await Future.delayed(Duration(seconds: 2));
     user = UserModel(
       name: "REDRODRIGO",
       photoUrl: "https://avatars.githubusercontent.com/u/31713982?v=4",
     );
+    state = HomeState.sucess;
   }
 
-  void getQuizzers() {
+  void getQuizzers() async {
+    state = HomeState.loading;
+    await Future.delayed(Duration(seconds: 2));
     quizzes = [
       QuizModel(
           imagem: AppImages.blocks,
@@ -40,5 +47,6 @@ class HomeController {
             ]),
           ])
     ];
+    state = HomeState.sucess;
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:DevQuiz/core/core.dart';
 import 'package:DevQuiz/home/home_controller.dart';
+import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -16,60 +18,72 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    controller.getUser();
-    controller.getQuizzers();
+    setState(() {
+      controller.getUser();
+      controller.getQuizzers();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBarWidget(
-          user: controller.user!,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 24,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(
-                    label: "Fácil",
-                  ),
-                  LevelButtonWidget(
-                    label: "Médio",
-                  ),
-                  LevelButtonWidget(
-                    label: "Difícil",
-                  ),
-                  LevelButtonWidget(
-                    label: "Perito",
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              Expanded(
-                child: GridView.count(
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  crossAxisCount: 2,
-                  children: controller.quizzes!
-                      .map((e) => QuizCardWidget(
-                            title: e.title,
-                            percent: e.questionAnswered/e.questions.length,
-                            completed:
-                                "${e.questionAnswered}/${e.questions.length}",
-                          ))
-                      .toList(),
-                ),
-              )
-            ],
+    if (controller.state == HomeState.sucess) {
+      return Scaffold(
+          appBar: AppBarWidget(
+            user: controller.user!,
           ),
-        ));
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(
+                      label: "Fácil",
+                    ),
+                    LevelButtonWidget(
+                      label: "Médio",
+                    ),
+                    LevelButtonWidget(
+                      label: "Difícil",
+                    ),
+                    LevelButtonWidget(
+                      label: "Perito",
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    crossAxisCount: 2,
+                    children: controller.quizzes!
+                        .map((e) => QuizCardWidget(
+                              title: e.title,
+                              percent: e.questionAnswered / e.questions.length,
+                              completed:
+                                  "${e.questionAnswered}/${e.questions.length}",
+                            ))
+                        .toList(),
+                  ),
+                )
+              ],
+            ),
+          ));
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
   }
 }
