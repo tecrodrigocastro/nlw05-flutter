ca244d17bb46672962db869366ee0eabd1ae2aac
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2021-04-22 14:45:16 -0300

    Challenge button page

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/challenge/widgets/Question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,28 @@ class _ChallengePageState extends State<ChallengePage> {
         child: SafeArea(top: true, child: QuestionIndicatorWidget()),
       ),
       body: QuizWidget(title: "O que o flutter faz em sua totalidade?"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            Expanded(
+                child: NextButtonWidget.white(
+              label: "Pular",
+              onTap: () {},
+            )),
+            SizedBox(
+              width: 7,
+            ),
+            Expanded(
+                child: NextButtonWidget.green(
+              label: "Confirmar",
+              onTap: () {},
+            )),
+          ]),
+        ),
+      ),
     );
   }
 }
